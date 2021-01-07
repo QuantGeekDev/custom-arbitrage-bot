01dd49d89d119d727c80e7ba4c9225cb0371e321
Daniel Tan <danieltanjiawang@gmail.com>
2021-01-07 16:19:37 +0800

    (fix) outstanding conflicts

--- hummingbot/core/utils/estimate_fee.py
+++ hummingbot/core/utils/estimate_fee.py
@@ -4,22 +4,6 @@ from hummingbot.client.config.fee_overrides_config_map import fee_overrides_conf
 from hummingbot.client.settings import CONNECTOR_SETTINGS
 from hummingbot.core.utils.eth_gas_station_lookup import get_gas_price, get_gas_limit
 
-<<<<<<< HEAD
-=======
-default_cex_estimate = {
-    # exchange: [maker_fee, taker_fee]
-    "binance": [0.1, 0.1],
-    "bitstamp": [0.5, 0.5],
-    "bittrex": [0.25, 0.25],
-    "coinbase_pro": [0.5, 0.5],
-    "huobi": [0.2, 0.2],
-    "kraken": [0.16, 0.26],
-    "kucoin": [0.1, 0.1],
-    "liquid": [0.1, 0.1],
-    "eterbase": [0.35, 0.35],
-    "crypto_com": [0.1, 0.1]
-}
->>>>>>> eacc1fba... Implemented bitstamp connector
 
 def estimate_fee(exchange: str, is_maker: bool) -> TradeFee:
     if exchange not in CONNECTOR_SETTINGS:
