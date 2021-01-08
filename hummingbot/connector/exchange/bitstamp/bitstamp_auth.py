1318f698a3215fef4aa0264fea383fbdfa2e0b3c
Daniel Tan <danieltanjiawang@gmail.com>
2021-01-08 17:10:27 +0800

    (refactor) BitstampAuth to use v2 endpoints and conventions

--- hummingbot/connector/exchange/bitstamp/bitstamp_auth.py
+++ hummingbot/connector/exchange/bitstamp/bitstamp_auth.py
@@ -1,33 +1,66 @@
 import hashlib
 import hmac
-from hummingbot.connector.exchange.bitstamp.bitstamp_tracking_nonce import get_tracking_nonce
-from typing import Dict
+import uuid
+import time
+
+from typing import Dict, Any
+from urllib.parse import urlencode
 
 
 class BitstampAuth:
-    def __init__(self, client_id: str, api_key: str, secret_key: str):
-        self.client_id = client_id
+    def __init__(self, api_key: str, secret_key: str):
         self.api_key = api_key
         self.secret_key = secret_key
 
-    def generate_auth_dict(self) -> Dict[str, str]:
+    def generate_auth_dict(self,
+                           http_method: str,
+                           path_url: str,
+                           query: str = "",
+                           params: Dict[str, Any] = None
+                           ) -> Dict[str, str]:
         """
         Generates authentication signature and returns it in request parameters
         :return: a string of request parameters including the signature
         """
 
-        # Get next nonce
-        api_nonce: str = get_tracking_nonce()
-        # Compile message
-        message: str = api_nonce + self.client_id + self.api_key
+        nonce: str = str(uuid.uuid4())
+        timestamp: str = str(int(round(time.time() * 1000)))
+        content_type = ""
+
+        headers = {
+            'X-Auth': 'BITSTAMP ' + self.api_key,
+            'X-Auth-Nonce': nonce,
+            'X-Auth-Timestamp': timestamp,
+            'X-Auth-Version': 'v2',
+        }
+
+        if http_method == "POST":
+            # Defaults to dummy payload if params are not provided
+            payload = params if params else {'offset': '1'}
+            content_type = "application/x-www-form-urlencoded"
+            headers["Content-Type"] = content_type
+
+        message: str = "BITSTAMP" + " " + self.api_key + \
+                       http_method + \
+                       "www.bitstamp.net/api/v2/" + \
+                       path_url + \
+                       query + \
+                       content_type + \
+                       nonce + \
+                       timestamp + \
+                       "v2" + \
+                       urlencode(payload)
+
         # Calculate signature
-        api_signature: str = hmac.new(
+        signature: str = hmac.new(
             self.secret_key.encode(),
-            message.encode(),
-            hashlib.sha256).hexdigest().upper()
+            message.encode("utf-8"),
+            hashlib.sha256).hexdigest()
+        headers["X-Auth-Signature"] = signature
 
         return {
             "key": self.api_key,
-            "signature": api_signature,
-            "nonce": api_nonce
+            "message": message,
+            "headers": headers,
+            "params": urlencode(payload)
         }
