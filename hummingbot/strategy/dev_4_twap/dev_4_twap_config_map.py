596eb23771d7c94ade2a10ca2581410bdab467b3
Daniel Tan <daniel@hummingbot.io>
2021-05-11 11:37:51 +0800

    (fix) include prompt_on_new parameter for twap configs

--- hummingbot/strategy/dev_4_twap/dev_4_twap_config_map.py
+++ hummingbot/strategy/dev_4_twap/dev_4_twap_config_map.py
@@ -36,51 +36,60 @@ dev_4_twap_config_map = {
         ConfigVar(key="market",
                   prompt="Enter the name of the exchange >>> ",
                   validator=validate_exchange,
-                  on_validated=lambda value: required_exchanges.append(value)),
+                  on_validated=lambda value: required_exchanges.append(value),
+                  prompt_on_new=True),
     "market_trading_pair_tuple":
         ConfigVar(key="market_trading_pair_tuple",
                   prompt=trading_pair_prompt,
-                  validator=validate_market_trading_pair_tuple),
+                  validator=validate_market_trading_pair_tuple,
+                  prompt_on_new=True),
     "order_type":
         ConfigVar(key="order_type",
                   prompt="Enter type of order (limit/market) default is market >>> ",
                   type_str="str",
                   validator=lambda v: None if v in {"limit", "market", ""} else "Invalid order type.",
-                  default="market"),
+                  default="market",
+                  prompt_on_new=True),
     "order_amount":
         ConfigVar(key="order_amount",
                   prompt="What is your preferred quantity (denominated in the base asset, default is 1)? "
                          ">>> ",
                   default=1.0,
-                  type_str="float"),
+                  type_str="float",
+                  prompt_on_new=True),
     "is_buy":
         ConfigVar(key="is_buy",
                   prompt="Enter True for Buy order and False for Sell order (default is Buy Order) >>> ",
                   type_str="bool",
-                  default=True),
+                  default=True,
+                  prompt_on_new=True),
     "num_individual_orders":
         ConfigVar(key="num_individual_orders",
                   prompt="Into how many individual orders do you want to split this order? (Enter 10 to indicate 10 individual orders. "
                          "Default is 1)? >>> ",
                   type_str="float",
-                  default=1),
+                  default=1,
+                  prompt_on_new=True),
     "time_delay":
         ConfigVar(key="time_delay",
                   prompt="How many seconds do you want to wait between each individual order? (Enter 10 to indicate 10 seconds. "
                          "Default is 10)? >>> ",
                   type_str="float",
-                  default=10),
+                  default=10,
+                  prompt_on_new=True),
     "order_price":
         ConfigVar(key="order_price",
                   prompt="What is the price of the limit order ? >>> ",
                   required_if=lambda: dev_4_twap_config_map.get("order_type").value == "limit",
-                  type_str="float"),
+                  type_str="float",
+                  prompt_on_new=True),
     "cancel_order_wait_time":
         ConfigVar(key="cancel_order_wait_time",
                   prompt="How long do you want to wait before cancelling your limit order (in seconds). "
                          "(Default is 60 seconds) ? >>> ",
                   required_if=lambda: dev_4_twap_config_map.get("order_type").value == "limit",
                   type_str="float",
-                  default=60),
+                  default=60,
+                  prompt_on_new=True),
 
 }
