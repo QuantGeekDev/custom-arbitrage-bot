28c6972f5fb25ccea2811dc75c96e3584f05abeb
Dennis Ocaña <50150287+dennisocana@users.noreply.github.com>
2021-07-15 10:26:09 +0800

    Revert "(refactor) update data_types for API Throttler"
    This reverts commit 44fb7e452a40c94ff5b749a4abfc42ef56c0dc12.

--- hummingbot/core/api_throttler/data_types.py
+++ hummingbot/core/api_throttler/data_types.py
@@ -1,25 +1,23 @@
 from dataclasses import dataclass
 
-DEFAULT_PATH = ""
-DEFAULT_WEIGHT = 1
-
-
-Limit = int             # Integer representing the no. of requests be time interval
-RequestPath = str       # String representing the request path url
-RequestWeight = int     # Integer representing the request weight of the path url
-Seconds = float
-
 
 @dataclass
 class RateLimit():
-    limit: Limit
-    time_interval: Seconds
-    path_url: RequestPath = DEFAULT_PATH
-    weight: RequestWeight = DEFAULT_WEIGHT
+    limit: int
+    time_interval: float
+    path_url: str = ""
+    weight: int = 1
 
 
 @dataclass
 class TaskLog():
     timestamp: float
-    path_url: RequestPath = DEFAULT_PATH
-    weight: RequestWeight = DEFAULT_WEIGHT
+    path_url: str = ""
+    weight: int = 1
+
+
+Limit = int             # Integer representing the no. of requests be time interval
+RequestPath = str       # String representing the request path url
+RequestWeight = int     # Integer representing the request weight of the path url
+
+Seconds = float
