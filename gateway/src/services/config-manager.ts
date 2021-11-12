9d5942f63f811b440920b908434268fbda162d55
james-hummingbot <james@hummingbot.io>
2021-11-12 22:20:49 +0100

    temp remove permission requirements

--- gateway/src/services/config-manager.ts
+++ gateway/src/services/config-manager.ts
@@ -37,26 +37,14 @@ export namespace ConfigManager {
   }
 
   export function readPassphrase(): string {
-    const mode = fs.lstatSync(passphraseFliePath).mode;
-    // check and make sure the passphrase file is a regular file, and is only accessible by the user.
-    if (
-      mode ===
-      (fs.constants.S_IFREG | fs.constants.S_IWUSR | fs.constants.S_IRUSR)
-    ) {
-      const x = yaml.load(
-        fs.readFileSync(passphraseFliePath, 'utf8')
-      ) as PassphraseConfig;
-      if ('CERT_PASSPHRASE' in x) {
-        return x.CERT_PASSPHRASE;
-      } else {
-        throw new Error(
-          passphraseFliePath + ' does not have CERT_PASSPHRASE set.'
-        );
-      }
+    const x = yaml.load(
+      fs.readFileSync(passphraseFliePath, 'utf8')
+    ) as PassphraseConfig;
+    if ('CERT_PASSPHRASE' in x) {
+      return x.CERT_PASSPHRASE;
     } else {
       throw new Error(
-        passphraseFliePath +
-          ' file does not strictly have mode set to user READ_WRITE only.'
+        passphraseFliePath + ' does not have CERT_PASSPHRASE set.'
       );
     }
   }
